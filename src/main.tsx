6d0ac70ce162e1a03e9a4d0d6ba72198ed3917eb
Frontend <frontenddev747@gmail.com>
2026-03-16 12:40:55 +0300

    add ScrollToTop

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./pages/App";
+import { ScrollToTop } from "./hooks/useScrollToTop";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
